9b5e227ad523b5606b95ec931c5b420bf7c8a6c4
Kadmo <kadmohardy@gmail.com>
2021-01-11 10:57:42 -0300

    prepare for gigalixir 3

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+echo "Start Gigalixir Deploy"
+
+git remote add gigalixir https://kadmothadeu%40gmail.com:f06056e5-f78a-472a-b9d0-b6c935574db6@git.gigalixir.com/blogkadmo.git
+git push gigalixir master
+
+echo "Finish Gigalixir Deploy"
