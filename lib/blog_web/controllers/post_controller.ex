09ad072e757e21c2106e9539063262a5a302ed95
Kadmo <kadmohardy@gmail.com>
2021-01-22 17:22:15 -0300

    add tests

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,14 +1,14 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
-  alias Blog.Posts.Post
+  alias Blog.{Posts, Posts.Post}
 
   def index(conn, _params) do
-    posts = Blog.Repo.all(Post)
+    posts = Posts.list_posts()
     render(conn, "index.html", posts: posts)
   end
 
   def show(conn, %{"id" => id}) do
-    post = Blog.Repo.get!(Post, id)
+    post = Posts.get_post!(id)
     render(conn, "show.html", post: post)
   end
 
@@ -17,7 +17,42 @@ defmodule BlogWeb.PostController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, _params) do
-    render(conn, "create.html")
+  def edit(conn, %{"id" => id}) do
+    post = Posts.get_post!(id)
+    changeset = Post.changeset(post)
+
+    render(conn, "edit.html", post: post, changeset: changeset)
+  end
+
+  def create(conn, %{"post" => post}) do
+    case Posts.create_post(post) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post created successfuly!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    Posts.delete_post(id)
+
+    conn
+    |> put_flash(:info, "Post foi deletado")
+    |> redirect(to: Routes.post_path(conn, :index))
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    case Posts.update_post(id, post_params) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post created successfuly!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
---
+++ test/blog/post_test.exs
@@ -0,0 +1,47 @@
+defmodule Blog.PostTest do
+  use Blog.DataCase
+  alias Blog.{Posts, Posts.Post}
+
+  @valid_post %{
+    title: "Phoenix Framework",
+    description: "Lorem"
+  }
+
+  @valid_post_update %{
+    title: "Phoenix Framework2",
+    description: "Lorem2"
+  }
+
+  def post_fixture(_attrs \\ %{}) do
+    {:ok, post} = Posts.create_post(@valid_post)
+    post
+  end
+
+  test "list_posts/0 return all posts" do
+    post = post_fixture()
+    assert Posts.list_posts() == [post]
+  end
+
+  test "get_posts/1 return all posts" do
+    post = post_fixture()
+    assert Posts.get_post!(post.id) == post
+  end
+
+  test "create_post/1 with valid data" do
+    assert = {:ok, %Post{} = post} = Posts.create_post(@valid_post)
+    assert post.title == "Phoenix Framework"
+    assert post.description == "Lorem"
+  end
+
+  test "update_post/2 with valid data" do
+    post = post_fixture()
+    assert = {:ok, %Post{} = post} = Posts.update_post(post.id, @valid_post_update)
+    assert post.title == "Phoenix Framework2"
+  end
+
+  test "delete_post/1" do
+    post = post_fixture()
+    assert {:ok, %Post{} = post}  = Posts.delete_post(post.id)
+    assert_raise Ecto.NoResultsError, fn -> Posts.get_post!(post.id) end
+  end
+end
