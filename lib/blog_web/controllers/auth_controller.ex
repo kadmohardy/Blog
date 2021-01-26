0d615571b5ee70c7a507cf4fdb398de2949144eb
Kadmo <kadmohardy@gmail.com>
2021-01-26 13:31:51 -0300

    create auth feature

--- lib/blog_web/controllers/auth_controller.ex
+++ lib/blog_web/controllers/auth_controller.ex
@@ -1,12 +1,17 @@
 defmodule BlogWeb.AuthController do
   use BlogWeb, :controller
+
   plug Ueberauth
 
-  def request(conn, _params) do
-    render(conn, "index.html")
-  end
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    user = %{token: auth.credentials.token,
+      email: auth.info.email,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      image: auth.info.image,
+      provider: provider
+    }
 
-  def callback(conn, _params) do
     render(conn, "index.html")
   end
 end
